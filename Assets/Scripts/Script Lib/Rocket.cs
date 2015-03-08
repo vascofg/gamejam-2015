6d0964e57b713f20905ff5bf2ab37b08062ecf97
ruiG <rui.nf1@gmail.com>
2015-03-08 14:30:52 +0000

    Teddies with animations

--- Assets/Scripts/Script Lib/Rocket.cs
+++ Assets/Scripts/Script Lib/Rocket.cs
@@ -9,17 +9,17 @@ public class Rocket : MonoBehaviour
 	void Start () 
 	{
 		// Destroy the rocket after 2 seconds if it doesn't get destroyed before then.
-		Destroy(gameObject, 2);
+		Destroy(gameObject, 1);
 	}
 
 
 	void OnExplode()
 	{
 		// Create a quaternion with a random rotation in the z-axis.
-		Quaternion randomRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+	//	Quaternion randomRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
 
 		// Instantiate the explosion where the rocket is with the random rotation.
-		Instantiate(explosion, transform.position, randomRotation);
+	//	Instantiate(explosion, transform.position, randomRotation);
 	}
 	
 	void OnTriggerEnter2D (Collider2D col) 
